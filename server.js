93cb7cbb5b3034b0db4f466f33f10e7887234d36
Luis <luis.gsn@outlook.com>
2020-08-11 04:35:00 -0400

    updating

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const Product = mongoose.model('Product');
 
 app.get('/', (req, res) => {
     Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
+        title: 'NodeJS',
+        description: 'Build native apps with Node',
+        url: 'http://github.com/facebook/node-js'
     });
 
     return res.send('Hello guys!');
